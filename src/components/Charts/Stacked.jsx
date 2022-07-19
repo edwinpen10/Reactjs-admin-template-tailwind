bfa1bc2a452579e63249f62cf39841df5087724c
edwinpen0101 <edwinpen010@gmail.com>
2022-07-19 15:18:22 +0700

    update

---
+++ src/components/Charts/Stacked.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+const Stacked = ({width, height}) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      Legend={{background:'white'}}
+    >
+       <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+
+       <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
+}
+
+export default Stacked
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../contexts/ContextProviders";
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={() => customFunc()}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
+      {icon}
+    </button>
+  </TooltipComponent>
+);
+
+const Navbar = () => {
+  const { 
+    activeMenu, setActiveMenu,
+     isClicked, setIsClicked,
+    screecSize, setScreecSize,
+    handleClick,currentColor        
+} = useStateContext();
+
+useEffect(() => {
+    const handleResize = () => setScreecSize(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+},[]);
+
+
+useEffect(() => {
+    if(screecSize <= 900) {
+        setActiveMenu(false)
+    }else{
+        setActiveMenu(true)
+    }
+},[screecSize]);
+
+  return (
+    <>
+      <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+        <NavButton
+          title="Menu"
+          customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+          color={currentColor}
+          icon={<AiOutlineMenu />}
+        />
+
+        <div className="flex">
+          <NavButton
+            title="Cart"
+            customFunc={() =>
+             handleClick('cart')}
+            color={currentColor}
+            icon={<FiShoppingCart />}
+          />
+           <NavButton
+            title="Chat"
+            customFunc={() =>
+             handleClick('chat')}
+            color={currentColor}
+            dotColor="#03C9D7"
+            icon={<BsChatLeft />}
+          />
+          <NavButton
+            title="Notification"
+            customFunc={() =>
+             handleClick('notification')}
+            color={currentColor}
+            dotColor="#03C9D7"
+            icon={<RiNotification3Line />}
+          />
+          <TooltipComponent content="User Profile" position="BottomCenter">
+                <div className="flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+                 onClick={() => handleClick('userProfile')}
+                >
+                <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Edwin
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+                </div>
+          </TooltipComponent>
+
+        {isClicked.cart && (<Cart />)}
+        {isClicked.chat && (<Chat />)}
+        {isClicked.notification && (<Notification />)}
+        {isClicked.userProfile && (<UserProfile />)}
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
